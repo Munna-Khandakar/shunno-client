d7d010573eaaf53358da8193fb907cedf45c9880
Munna-Khandakar <munnashisad@gmail.com>
2025-06-07 22:53:02 +0600

    logo added

--- src/app/dashboard/transection/page.tsx
+++ src/app/dashboard/transection/page.tsx
@@ -1,18 +1,18 @@
-import PageContainer from '@/components/layout/page-container';
-import { buttonVariants } from '@/components/ui/button';
-import { Heading } from '@/components/ui/heading';
-import { Separator } from '@/components/ui/separator';
-import { DataTableSkeleton } from '@/components/ui/table/data-table-skeleton';
-import TransectionListingPage from '@/features/products/components/product-listing';
-import { searchParamsCache } from '@/lib/searchparams';
-import { cn } from '@/lib/utils';
-import { IconPlus } from '@tabler/icons-react';
-import Link from 'next/link';
-import { SearchParams } from 'nuqs/server';
-import { Suspense } from 'react';
+import PageContainer from "@/components/layout/page-container";
+import { buttonVariants } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTableSkeleton } from "@/components/ui/table/data-table-skeleton";
+import TransectionListingPage from "@/features/products/components/product-listing";
+import { searchParamsCache } from "@/lib/searchparams";
+import { cn } from "@/lib/utils";
+import { IconPlus, IconMinus } from "@tabler/icons-react";
+import Link from "next/link";
+import { SearchParams } from "nuqs/server";
+import { Suspense } from "react";
 
 export const metadata = {
-  title: 'Dashboard: Transections'
+  title: "Dashboard: Transections",
 };
 
 type pageProps = {
@@ -29,24 +29,26 @@ export default async function Page(props: pageProps) {
 
   return (
     <PageContainer scrollable={false}>
-      <div className='flex flex-1 flex-col space-y-4'>
-        <div className='flex items-start justify-between'>
+      <div className="flex flex-1 flex-col space-y-4">
+        <div className="flex items-start justify-between">
           <Heading
-            title='Transections'
-            description='Manage Transections (Server side table functionalities.)'
+            title="Transections"
+            description="Manage Transections (Server side table functionalities.)"
           />
-          <Link
-            href='/dashboard/transection/new'
-            className={cn(buttonVariants(), 'text-xs md:text-sm')}
-          >
-            <IconPlus className='mr-2 h-4 w-4' /> Add New
-          </Link>
-          <Link
-            href='/dashboard/transection/new'
-            className={cn(buttonVariants(), 'text-xs md:text-sm')}
-          >
-            <IconPlus className='mr-2 h-4 w-4' /> Add New
-          </Link>
+          <div>
+            <Link
+              href="/dashboard/transection/new"
+              className={cn(buttonVariants({variant: 'outline'}), "text-xs md:text-sm mr-2")}
+            >
+              <IconMinus className="mr-2 h-4 w-4" /> Add Expense
+            </Link>
+            <Link
+              href="/dashboard/transection/new"
+              className={cn(buttonVariants(), "text-xs md:text-sm")}
+            >
+              <IconPlus className="mr-2 h-4 w-4" /> Add Income
+            </Link>
+          </div>
         </div>
         <Separator />
         <Suspense
